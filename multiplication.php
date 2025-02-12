96116ae38bc8016ad2c3351e55f6daf3d945a479
Victory of Divine <152266134+Debjit69@users.noreply.github.com>
2025-02-12 11:43:23 +0530

    Add files via upload

---
+++ multiplication.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>multiplication table</title>
+        </head>
+        <body>
+            <form action="multiplicationtable.php" method="post">
+                <label>enter a number:</label><br><br>
+                <input type="number" name="myname" required><br><br>
+                <input type="reset"><br><br>
+                <input type="submit" value="submit">
+                </form>
+                </body>
+                </html>
---
+++ multiplicationtable.php
@@ -0,0 +1,6 @@
+<?php
+$n=$_POST['myname'];
+for($i=1;$i<=10;$i++){
+    echo $i."x".$n."=".($i*$n)."<br>";
+}
+?>
