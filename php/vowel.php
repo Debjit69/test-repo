aadcc561606676a2f39c77b60d49a3b71f3b81f1
Victory of Divine <152266134+Debjit69@users.noreply.github.com>
2025-02-24 14:41:56 +0530

    Add files via upload

---
+++ php/vowel.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>vowel count</title>
+        </head>
+        <body>
+            <form action="vowelcount.php" method="post">
+                <label>enter a string:</label><br><br>
+                <input type="text " name="string" required><br><br>
+                <input type="reset"><br><br>
+                <input type="submit" value="submit">
+                </form>
+                </body>
+                </html>
