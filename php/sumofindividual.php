c17dccec37d2c5e2a799dfea40a82ccb90a29845
Victory of Divine <152266134+Debjit69@users.noreply.github.com>
2025-02-26 11:59:38 +0530

    Add files via upload

---
+++ php/sumofindividual.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sum of individual</title>
+</head>
+<body>
+   <form action="./findsumofindi.php" method="post">
+    <label>enter a number:</label><br><br>
+    <input type="number" name="number" required><br><br>
+    <input type="reset"><br><br>
+    <input type="submit" value="submit">
+    </form> 
+</body>
+</html>
